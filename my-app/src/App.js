9ca9400a64114737ef028bbf2372cffd52566c9f
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-10-18 12:58:49 +0300

    Build: Add and configure the react-bootstrap framework as well as scss
    I have configured and integrated the react-bootstrap framework alongside scss. I have also gave an example how you can use the scss and add new custom styles.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './rescrap.scss';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <button className="btn btn-primary-green">hello</button>
     </div>
   );
 }
