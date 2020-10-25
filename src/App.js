7f905248d4ec6d92dabdcecec7cd2c76e8820c95
Arhab Al-Shargabi <arhab1000000@gmail.com>
2020-10-25 10:12:20 +0300

    Feat: Add a footer component
    I have bulid a footer that made of 4 columns the first is about a small info about the company the second is about the product and how to get and buy them the third one is about contacting such as About us , FAQ and the last one is about selling and earn money and how to become a partner.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './styles/main.css';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <div className="App">
-      <p className="text-black">learn react</p>
+      <Footer />
     </div>
   );
 }
