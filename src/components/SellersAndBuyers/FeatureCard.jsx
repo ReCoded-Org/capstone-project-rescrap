9d136202c1f0f4a3eaf5151721308546220d93c7
Tahany-Alswoidy <tahaniswoidy1232018@gmail.com>
2020-11-04 17:24:50 +0300

    Refactor: refactor the pages
    remove commented code, remove console logs, add constants for repeated
    classes to satisfy DRY principle.

--- src/components/SellersAndBuyers/FeatureCard.jsx
+++ src/components/SellersAndBuyers/FeatureCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Title from '../Title/Title';
+import SubTitle from '../SubTitle/SubTitle';
 import Description from '../Description/Description';
 
-const FeatureCard = ({imgPath, imgDesc, featHeader, featDesc}) => {
-    return ( <div className="w-64 sm\:pb-64 text-center">
+const FeatureCard = ({imgPath, imgDesc, featHeader, featDesc, classes}) => {
+    return ( <div className={"w-64 sm\:pb-16 text-center "+classes}>
         <img className="h-24 w-24 inline-block bg-green-400" src={imgPath} alt={imgDesc}/>
-        <Title classes=" my-3" text={featHeader}/>
+        <SubTitle classes=" my-3" text={featHeader}/>
         <Description classes="" text={featDesc}/>
     </div> );
 }
