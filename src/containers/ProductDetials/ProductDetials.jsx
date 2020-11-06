5710f675491bcaa7d24619bffcee16bb97705af2
malthawr <malthawr@gmail.com>
2020-11-06 16:29:01 +0300

    Feat:build the product details page and import the files in it also fix the paths of the tests

---
+++ src/containers/ProductDetials/ProductDetials.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Intro from './Intro';
+import Info from './Info';
+import SuggestedProducts from './SuggestedProducts';
+
+const ProductsDetials= () => {
+    
+  return (
+  <Intro  />
+  <Info />
+  <SuggestedProducts />
+    );
+}
+
+export default ProductsDetials;
