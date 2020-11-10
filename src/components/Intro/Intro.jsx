95bfa8695c95e32f33f5f1f892fbc2ff5bfb596c
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-11-10 12:03:12 +0300

    Build: add multi language package i18n
    For more info about how to use the pack get back to:
    https://react.i18next.com/latest/using-with-hooks

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import Title from '../Title/Title';
 import Description from '../Description/Description';
-import Butten from '../Button/Button';
+import Button from '../Button/Button';
 import './Intro.css'
 
 const Intro = ({nav}) => {
@@ -12,7 +12,7 @@ const Intro = ({nav}) => {
     <div className="intro-content p-12 py-24">
         <Title classes=" text-green-200 my-4" text="Recycling Marketplace"/>
         <Description classes=" text-lg my-4" text="Connect and trade directly with suppliers & buyers of recyclable plastics" />
-        <Butten btnClasses=" bg-green-200 text-white my-4" btnClickHandler='' btnText="Sign up" />
+        <Button btnClasses=" bg-green-200 text-white my-4 " btnClickHandler='' btnText="Sign up" />
     </div> </div>);
 }
  
