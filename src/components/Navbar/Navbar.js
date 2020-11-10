1d263563e4d50bb11a2d3c8f43b507f7c80592b0
Tahany-Alswoidy <tahaniswoidy1232018@gmail.com>
2020-11-10 10:50:05 +0300

    Feat: improve the home page Navbar
    improve the responsiveness features of the navbar,
    add a connection between the Add Product element and
    a user that has account

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,20 +3,29 @@ import {Link} from 'react-router-dom';
 import RescrapLogo from './RescrapLogo';
 import NavbarItems from './NavbarItems';
 import './Navbar.css';
-const Navbar = () => {
+const Navbar = ({atHome, hasAccount}) => {
   const [navbar, setNavbar] = useState(false);
   const [signin, setSignin] = useState(false);
+  const [humbergur, setHumbergur]=useState("fill-current text-white");
   const changeBackground = () => {
     if (window.scrollY >= 50) {
       setNavbar(true);
+      setHumbergur("fill-current text-green-200");
     } else {
       setNavbar(false);
+      setHumbergur("fill-current text-white");
     }
   };
   window.addEventListener('scroll', changeBackground);
+let items=NavbarItems;
+
+if(!hasAccount && atHome){
+  items=NavbarItems.filter(item=>item.label!=='Add Product');
+}
+
   return (
     <section className={navbar ? 'navbar active' : 'navbar'}>
-      <div className="container mx-auto px-4 py-2 pt-4 flex justify-between flex-wrap">
+      <div className="container mx-auto px-4 py-2 pt-4 flex justify-between flex-wrap bg-opacity-50">
         <Link to="/">
           <RescrapLogo />
         </Link>
@@ -25,7 +34,7 @@ const Navbar = () => {
           className="block md:hidden cursor-pointer self-center mr-2 menuIcon"
         >
           <svg
-            className="fill-current text-green-200 "
+            className={humbergur}
             xmlns="http://www.w3.org/2000/svg"
             width="20"
             height="20"
@@ -38,7 +47,7 @@ const Navbar = () => {
         <input class="hidden" type="checkbox" id="menu-toggle" />
         <nav className="hidden w-full text-center md:flex md:w-auto" id="menu">
           <ul className="pt-5 mb-5 md:mb-0 md:pt-0 md:flex">
-            {NavbarItems.map((item, index) => {
+            {items.map((item, index) => {
               return (
                 <li
                   className="py-2 mx-4 transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300"
