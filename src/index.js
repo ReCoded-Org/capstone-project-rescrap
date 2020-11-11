13a036636081afa2e3473989b9bbb1d24f74d87c
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-11-11 21:41:03 +0300

    Build: Add i18next package and configure it.
    I have added the i18next translation package to our project and configured it. I also prepared the translation files.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-
+import './i18n';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Suspense fallback="Loading...">
+      <Router>
+        <App />
+      </Router>
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 );
