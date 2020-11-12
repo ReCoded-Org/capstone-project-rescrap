e6e69ea984dce67516dad71f168ab00614f2667f
Arhab Al-Shargabi <arhab1000000@gmail.com>
2020-11-12 10:15:08 +0300

    Doc: Write a proper content for the website

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -10,7 +10,7 @@ const Intro = ({nav}) => {
     return ( <div className="intro h-screen">
       <div>{nav}</div>
     <div className="intro-content p-12 py-24">
-        <Title classes=" text-green-200 my-4" text="Recycling Marketplace"/>
+        <Title classes=" text-green-200 my-4" text="Scrap Recycling Marketplace"/>
         <Description classes=" text-lg my-4" text="Connect and trade directly with suppliers & buyers of recyclable plastics" />
         <Button btnClasses=" bg-green-200 text-white my-4 " btnClickHandler='' btnText="Sign up" />
     </div> </div>);
