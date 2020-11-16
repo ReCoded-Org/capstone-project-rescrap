676b0ce1e38dba9a1bde8f8a01d401c6ab0fdcdd
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-11-17 00:11:55 +0300

    Fix: Fix responsiveness issue of about us page
    I have fixed about us page and updaed its test files.

--- src/containers/About-us/About-us-info.jsx
+++ src/containers/About-us/About-us-info.jsx
@@ -6,15 +6,15 @@ import { useTranslation } from 'react-i18next';
 
 const AboutUsInfo = () => {
     const {t}=useTranslation();
-    return <div>
+    return <div className="">
         <SubTitle
         text= { t('translation:pages.aboutus.subtitle') }
         classes='text-5xl text-center text-green-100 m-4 ' />
-        <div className='text-base flex mx-8 mb-16'>
-            <p>
+        <div className='text-base flex flex-col md:flex-row lg:flex-row xl:flex-row mx-8 mb-16'>
+            <p className="p-8 text-justify">
                 { t('translation:pages.aboutus.paragraph1') }
                    </p>
-                    <p>{ t('translation:pages.aboutus.paragraph2') }
+                    <p className="p-8 text-justify">{ t('translation:pages.aboutus.paragraph2') }
 
                    </p>
             </div>
