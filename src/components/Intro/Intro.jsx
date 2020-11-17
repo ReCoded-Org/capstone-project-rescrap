83e5aafdfa9891390a819d3a09ebd9d837e63bc1
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-11-17 09:36:40 +0300

    Style: change style of home page intro when changing language
    When changing language the text becomes white and button bg-white

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import Title from '../Title/Title';
 import Description from '../Description/Description';
 import Button from '../Button/Button';
@@ -8,13 +8,25 @@ import { useTranslation } from 'react-i18next';
  
 
 const Intro = ({nav,signIn}) => {
-  const {t}=useTranslation();
+
+  const {t,i18n}=useTranslation();
+  const [color,setColor]=useState("green-200");
+  const [btnTextColor,setBtnTextColor]=useState("white")
+  useEffect(() => {
+   if(i18n.language==="ar"){
+     setColor("white");
+     setBtnTextColor("green-200");
+   }else{
+     setColor("green-200");
+     setBtnTextColor("white")
+   }
+  }, [t,i18n.language])
     return ( <div className="intro h-screen">
       <div>{nav}</div>
     <div className="intro-content p-12 py-24">
-        <Title classes=" text-green-200 my-4" text={ t('translation:pages.home.title') }/>
+        <Title classes={" text-"+color+ " my-4"} text={ t('translation:pages.home.title') }/>
         <Description classes=" text-lg my-4" text={ t('translation:pages.home.paragraph') } />
-        <Button btnClasses=" bg-green-200 text-white my-4 " btnClickHandler={signIn} btnText={ t('translation:pages.home.sginup-btn') } />
+        <Button btnClasses={" bg-"+color+" text-"+btnTextColor+" my-4 "} btnClickHandler={signIn} btnText={ t('translation:pages.home.sginup-btn') } />
     </div> </div>);
 }
  
